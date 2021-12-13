8f01a4b088821e8fb01de04ae29cba4d63d86893
jahanzaib <jahanzaib@clustox.com>
2021-12-14 01:46:47 +0500

    chat component is completed

--- src/Admin-Main-View/Chat_Settings/ChatSettings.js
+++ src/Admin-Main-View/Chat_Settings/ChatSettings.js
@@ -16,58 +16,12 @@ import {
 import { PlusOutlined, SmileOutlined, UserOutlined } from "@ant-design/icons";
 import "./index.css";
 import "../../App.css";
+import TagsComponent from "./TagsComponent";
 import Question from "./Question";
 const Settings_Component = (props) => {
-  const [openModal, setOpenModal] = useState(true);
   const { Panel } = Collapse;
 
-  const [tags, setTags] = useState(["Unremovable", "Tag 2", "Tag 3"]);
-  const [inputVisible, setInputVisible] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const [editInputIndex, setEditInputIndex] = useState(-1);
-  const [editInputValue, setEditInputValue] = useState("");
-  const handleClose = (removedTag) => {
-    // const tags = this.state.tags.filter((tag) => tag !== removedTag);
-    // console.log(tags);
-    // this.setState({ tags });
-
-    setTags((tags) => tags.filter((x) => x !== removedTag));
-  };
-
-  const showInput = () => {
-    // this.setState({ inputVisible: true }, () => this.input.focus());
-    setInputVisible(true);
-  };
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-  //////////////////
-  const handleInputConfirm = () => {
-    if (inputValue && tags.indexOf(inputValue) === -1) {
-      // setTags[inputValue];
-    }
-  };
-
-  const handleEditInputChange = (e) => {
-    setEditInputValue(e.target.value);
-  };
-
-  const handleEditInputConfirm = () => {
-    this.setState(({ tags, editInputIndex, editInputValue }) => {
-      const newTags = [...tags];
-      newTags[editInputIndex] = editInputValue;
-    });
-  };
-  /////////////////
   const { TextArea } = Input;
-  const showModal = () => {
-    props.setOpenModal(true);
-  };
-
-  const handleOk = () => {
-    props.setOpenModal(false);
-  };
 
   const handleCancel = () => {
     props.setOpenModal(false);
@@ -100,42 +54,29 @@ const Settings_Component = (props) => {
             <Question text="Set 140 character text limit" />
             <Question text="Disable Emojis" />
           </div>
-          {tags.map((tag, index) => {
-            if (editInputIndex === index) {
-              return (
-                <Input
-                  // ref={this.saveEditInputRef}
-                  // key={tag}
-                  size="small"
-                  value={editInputValue}
-                  onChange={handleEditInputChange}
-                  onBlur={handleEditInputConfirm}
-                  onPressEnter={handleEditInputConfirm}
-                />
-              );
-            }
-          })}
-
-          <p
+          <div
             style={{
-              fontFamily: "Lato",
-              fontStyle: "normal",
-              fontWeight: "normal",
-              fontSize: "18px",
-              lineHeight: "22px",
-              /* identical to box height */
-
-              color: "#FFFFFF",
+              display: "flex",
+              flexDirection: "column",
+              marginTop: "10px",
             }}
           >
-            Blocked Words/Phrases
-          </p>
-          <Tag closable={true}>Helo </Tag>
-          <Tag closable={true}>Helo </Tag>
-          <Tag closable={true}>Helo </Tag>
-          <Tag className="site-tag-plus" onClick={showInput}>
-            <PlusOutlined /> New Tag
-          </Tag>
+            <p
+              style={{
+                fontFamily: "Lato",
+                fontStyle: "normal",
+                fontWeight: "normal",
+                fontSize: "18px",
+                lineHeight: "22px",
+                /* identical to box height */
+
+                color: "#FFFFFF",
+              }}
+            >
+              Blocked Words/Phrases
+            </p>
+            <TagsComponent />
+          </div>
         </div>
       </Modal>
     </div>
